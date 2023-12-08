3b258ffc33bcb089e85fc53665a5c80a00e5b3c7
sanyamk0 <sanyamkumar002@gmail.com>
2023-12-08 22:55:36 +0530

    integrated socket.io

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import userRouter from "./routes/user.js";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
-dotenv.config();
+const app = express();
+app.use(express.json()); // Use JSON parsing middleware to handle JSON requests
+app.use(cors()); // Enable CORS to allow requests from different origins
+const server = http.createServer(app);
 
-const server = express();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
-server.use(express.json()); // Use JSON parsing middleware to handle JSON requests
-server.use(cors()); // Enable CORS to allow requests from different origins
+io.on("connection", (socket) => {
+  console.log(`User Connected ${socket.id}`);
+  socket.on("created", (data) => {
+    console.log(data);
+  });
+  socket.on("joined", (data) => {
+    console.log(data);
+  });
+});
 
 //Database Connection
 mongoose
@@ -18,7 +35,7 @@ mongoose
   .catch((err) => {
     console.error(err.message);
   });
-server.use("/", userRouter); // Use the userRouter for handling routes starting from the root ("/") URL
+app.use("/", userRouter); // Use the userRouter for handling routes starting from the root ("/") URL
 
 server.listen(process.env.PORT, () => {
   console.log(`Server Started at Port ${process.env.PORT}`);
